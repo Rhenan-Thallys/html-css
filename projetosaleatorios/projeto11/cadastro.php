307dc93178b1723f43d8d2b8263e572a0dbdd179
Rhenan-Thallys <rhenanthallys04@gmail.com>
2026-04-10 20:59:57 -0300

    atualizei o projeto 10

--- projetosaleatorios/projeto11/cadastro.php
+++
@@ -1,3 +0,0 @@
-Seus dados não foram enviados!
-    
-Aprenda php para saber o que fazer com os dados..
--- projetosaleatorios/projeto12/cadastro.php
+++
@@ -1,4 +0,0 @@
-Seus dados não foram enviados...
-
-Aprenda php para descobrir o que fazer com eles!
-
